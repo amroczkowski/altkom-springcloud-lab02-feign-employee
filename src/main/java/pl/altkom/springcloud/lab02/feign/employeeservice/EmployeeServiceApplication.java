fb9454b0f30ad46d1d0e700c710b554a724276b5
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-05-22 23:15:55 +0200

    Init

---
+++ src/main/java/pl/altkom/springcloud/lab02/feign/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab02.feign.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/altkom/springcloud/lab02/feign/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab02.feign.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
